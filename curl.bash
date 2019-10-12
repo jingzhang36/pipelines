b05f6a85401e4feb9faf25651c52accce08e213c
jingzhang36 <jingzhangjz@google.com>
2019-10-12 17:36:07 +0800

    ci test

--- curl.bash
+++ curl.bash
@@ -6,5 +6,5 @@ data='{"name":'\""$1"\"', "package_url": {"pipeline_url": "https://storage.googl
 version=$(curl -H "Content-Type: application/json" -X POST -d "$data" http://35.192.214.113:8888/apis/v1beta1/pipeline_versions | jq -r ".id")
 
 # create run
-rundata='{"name":'\""$1-run"\"', "resource_references": [{"key": {"id": '\""$version"\"', "type":4}, "relationship":2}]}'
+rundata='{"name":'\""$1-run"\"', "resource_references": [{"key": {"id": '\""$version"\"', "type":4}, "relationship":2}, {"key": {"id": "52e9cbbd-1670-4486-967f-ff1577f3f9b1", "type":1}, "relationship": 1}]}'
 curl -H "Content-Type: application/json" -X POST -d "$rundata" http://35.192.214.113:8888/apis/v1beta1/runs
