5d19abf34f21428976f4adc91e46c7e81806e063
Jing Zhang <jingzhangjz@google.com>
2020-06-13 19:06:50 +0800

    name contains

--- backend/src/apiserver/resource/resource_manager_test.go
+++ backend/src/apiserver/resource/resource_manager_test.go
@@ -845,7 +845,7 @@ func TestCreateRun_WithOldestRunDeleted(t *testing.T) {
 	assert.Equal(t, 2, store.ArgoClientFake.GetWorkflowCount(), "Workflow count is not as expected.")
 	retrievedWorkflows, err := store.ArgoClientFake.GetWorkflows()
 	for _, wf := range retrievedWorkflows.Items {
-		assert.Contains(t, ["workflow1", "workflow2"], wf.Name)
+		assert.Contains(t, [2]string{"workflow1", "workflow2"}, wf.Name)
 	}
 
 	// expectedRuntimeWorkflow := testWorkflow.DeepCopy()
