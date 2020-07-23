4492e57e1fcf725c80197839c4b560bc6080a1c2
Jing Zhang <jingzhangjz@google.com>
2020-07-23 17:33:20 +0800

    remove the added filter

--- backend/src/apiserver/storage/experiment_store_test.go
+++ backend/src/apiserver/storage/experiment_store_test.go
@@ -60,22 +60,7 @@ func TestListExperiments_Pagination(t *testing.T) {
 		StorageState:   "STORAGESTATE_AVAILABLE",
 	}
 	experimentsExpected := []*model.Experiment{expectedExperiment1, expectedExperiment4}
-
-	filterProto := &api.Filter{
-		Predicates: []*api.Predicate{
-			&api.Predicate{
-				Key: "name",
-				Op:  api.Predicate_IN,
-				Value: &api.Predicate_StringValues{
-					StringValues: &api.StringValues{
-						Values: []string{"experiment1", "experiment2", "experiment3"},
-					},
-				},
-			},
-		},
-	}
-
-	opts, err := list.NewOptions(&model.Experiment{}, 2, "name", filterProto /*nil*/)
+	opts, err := list.NewOptions(&model.Experiment{}, 2, "name", nil)
 	assert.Nil(t, err)
 
 	experiments, total_size, nextPageToken, err := experimentStore.ListExperiments(&common.FilterContext{}, opts)
