199f8459cdac0c1219085819b9d98c66bce3ab5f
Jing Zhang <jingzhangjz@google.com>
2020-07-29 14:21:33 +0800

    try a metric in ml pipeline

--- backend/src/apiserver/main.go
+++ backend/src/apiserver/main.go
@@ -57,14 +57,14 @@ func recordMetrics() {
 	go func() {
 		for {
 			opsProcessed.Inc()
-			time.Sleep(2 * time.Second)
+			time.Sleep(30 * time.Second)
 		}
 	}()
 }
 
 var (
 	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "myapp_processed_ops_total",
+		Name: "a_metric_of_processed_ops_total",
 		Help: "The total number of processed events",
 	})
 )
